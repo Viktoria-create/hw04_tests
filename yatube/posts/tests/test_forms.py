a040dcc639f10c3d518db786f67895eda66cc9e1
Виктория Кошельник <viktoriya_arhilei@mail.ru>
2022-09-24 20:45:22 +0300

    Исправление

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -58,53 +58,51 @@ class PostFormTests(TestCase):
             text='Тестовый текст',
             author=self.user,
             group=self.group
-            )
+        )
         group2 = Group.objects.create(
             title='Тестовая группа2',
             slug='test_group',
             description='Описание'
-            )
+        )
         form_data = {
             'text': 'Текст записанный в форму',
             'group': group2.id
-            }
+        }
         response = self.authorized_client.post(
             reverse('posts:post_edit', kwargs={'post_id': old_text.id}),
             data=form_data,
             follow=True
-            )
+        )
         self.assertRedirects(
             response,
             reverse('posts:post_detail', kwargs={'post_id': old_text.id}),
-            )
+        )
         self.assertTrue(
             Post.objects.filter(
-            text=form_data['text'],
-            group=group2.id,
-            ).exists(),
+                text=form_data['text'],
+                group=group2.id,).exists(),
             f'Не найдена тестовая запись {form_data ["text"]} в БД'
-            )
+        )
         # не авторизованный пользователь не может изменить содержание поста'
         form_data = {
             'text': 'Неавторизованный текст записанный в форму',
             'group': group2.id
-            }
+        }
         response = self.guest_client.post(
             reverse('posts:post_edit', kwargs={'post_id': old_text.id}),
             data=form_data,
             follow=True
-            )
+        )
         self.assertRedirects(
             response,
             '/auth/login/?next=/posts/1/edit/'
-            )
+        )
         self.assertFalse(
             Post.objects.filter(
-            text=form_data['text'],        
-            group=group2.id,
-            ).exists(),
+                text=form_data['text'],
+                group=group2.id,).exists(),
             f'записи {form_data ["text"]} в БД быть не должно'
-            )
+        )
 
     def test_no_edit_post(self):
         """Проверка запрета добавления поста в базу данных
